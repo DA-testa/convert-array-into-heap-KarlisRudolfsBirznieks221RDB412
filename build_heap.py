ea57a7652dd2995befe8b3383866ca7172f88174
Kārlis Rūdolfs Birznieks <125714170+KarlisRudolfsBirznieks221RDB412@users.noreply.github.com>
2023-03-19 16:36:26 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,87 +1,62 @@
 # 221RDB412 Kārlis Rūdolfs Birznieks
 
-import os
-
-
 def build_heap(data):
     swaps = []
     n = len(data)
-    # TODO: Create heap and heap sort
-
-    # Add heap_type parameter to sift_down function
-    for i in range(n // 2, -1, -1):
-        swaps = sift_down(data, swaps, i, n, heap_type)
+    # Create heap
+    for i in range(n//2, -1, -1):
+        sift_down(i, data, swaps)
     return swaps
 
-
-def sift_down(data, swaps, i, n, heap_type):
-    while i < n:
-        left = 2 * i + 1
-        right = 2 * i + 2
-        smallest = i
-        # Modify the comparison based on the heap_type parameter
-        if left < n and ((data[left] < data[smallest]) if heap_type == "I" else (data[left] > data[smallest])):
-            smallest = left
-        if right < n and ((data[right] < data[smallest]) if heap_type == "I" else (data[right] > data[smallest])):
-            smallest = right
-        if smallest != i:
-            data[i], data[smallest] = data[smallest], data[i]
-            swaps.append((i, smallest))
-            i = smallest
-        else:
-            break
-    return swaps
-
+def sift_down(i, data, swaps):
+    n = len(data)
+    min_index = i
+    l = 2*i + 1
+    if l < n and data[l] < data[min_index]:
+        min_index = l
+    r = 2*i + 2
+    if r < n and data[r] < data[min_index]:
+        min_index = r
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        sift_down(min_index, data, swaps)
 
 def main():
+    # Prompt user for input type (F for file, I for keyboard)
+    option = input("Enter input type: ")
+    data = []
 
-    global heap_type
-
-    heap_type = input().strip().upper()
-    assert heap_type in ["I", "F"]
-
-    # Read input
-    if heap_type == "F":
+    if "F" in option:
         # Input from file
         try:
-            # Prompt user for file path and strip the carriage return character
-            file_path = input("Input file path: ").rstrip('\r')
-            # Use os to get the absolute path of the file
-            abs_path = os.path.abspath(file_path)
-            # Read input from file
-            with open(abs_path, "r") as file:
-                # Read the number of elements from the first line
+            file_path = input("Input file path: ")
+            with open(f"tests/{file_path}", "r") as file:
                 n = int(file.readline().strip())
-                # Read the list of elements from the second line
                 data = list(map(int, file.readline().strip().split()))
         except FileNotFoundError:
             print("File not found.")
             return
-    else:
+    elif "I" in option:
         # Input from keyboard
         try:
-            # Read the number of elements from the first line
-            n = int(input().strip())
-            # Read the list of elements from the second line
-            data = list(map(int, input().strip().split()))
+            n = int(input())
+            data = list(map(int, input().split()))
         except ValueError:
             print("Invalid input format.")
             return
+    else:
+        print("Invalid input type.")
+        return
 
-    # Check input length
     assert len(data) == n
 
-    # Call function to assess the data and give back all swaps
     swaps = build_heap(data)
 
-    # Output the heap type and how many swaps were made
-    print(heap_type)
+    # Output number of swaps and each swap
     print(len(swaps))
-
-    # Output all swaps
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
